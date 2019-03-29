d096d00e10ea24cb4d969d5b03c3707c3821f4d8
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-03-29 16:58:01 +0100

    Add some empty files for project structure

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import argparse
+
+argparser = argparse.ArgumentParser()
+
+argparser.add_argument(
+    "images",
+    help="List of paths to input images, left to right", 
+    nargs="+",
+    default=[])
+
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    args = argparser.parse_args()
+    images = args.images
