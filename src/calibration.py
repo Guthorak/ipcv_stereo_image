a7824b2cc19acd442b6185d36b36817e8f6ed590
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-04-01 13:40:58 +0200

    Implement camera calibration

--- src/calibration.py
+++ src/calibration.py
@@ -3,6 +3,11 @@ Module containing camera calibration utilities
 """
 import numpy as np
 import cv2
+from tqdm import tqdm
+
+
+class CalibrationError(Exception):
+    pass
 
 
 class Calibrator:
@@ -21,21 +26,81 @@ class Calibrator:
         images_right: list
             List of paths to right calibration images 
         """
-        self._images_left = images_left
-        self._images_middle = images_middle
-        self._images_right = images_right
+        self._images = {
+            "left": images_left,
+            "right": images_right,
+            "middle": images_middle
+        }
+        self._calibration_matrices = {}
+        self._translation = {}
+        self._rotation = {}
+        self._distortion_coefficients = {}
 
     def calibrate(self) -> None:
-        raise NotImplementedError
+        NROWS, NCOLS = 6, 9
+        for key, value in self._images.items():
+            print(f"Calibrating camera {key}")
+            imshape = cv2.cvtColor(cv2.imread(value[0]), cv2.COLOR_BGR2GRAY).shape[::-1]
+            object_points, image_points = self._get_object_and_image_points(NROWS, NCOLS, value)
+            success, matrix, distortion, rotation, translation = cv2.calibrateCamera(object_points, 
+                image_points, 
+                imshape,
+                None, 
+                None)
+            if not success:
+                raise CalibrationError(f"Failed to calibrate camera: {key}")
+            self._calibration_matrices[key] = matrix
+            self._translation[key] = translation
+            self._rotation[key] = rotation
+            self._distortion_coefficients[key] = distortion
 
-    def calibration_matrix(self) -> np.ndarray:
-        raise NotImplementedError
+    @property
+    def calibration_matrices(self) -> dict:
+        return self._calibration_matrices
 
-    def distortion_coefficients(self):
-        raise NotImplementedError
+    @property
+    def distortion_coefficients(self) -> dict:
+        return self._distortion_coefficients
 
-    def rotation(self):
-        raise NotImplementedError
+    @property
+    def rotations(self) -> dict:
+        return self._rotation
 
-    def translation(self):
-        raise NotImplementedError
+    def translations(self) -> dict:
+        return self._translation
+
+    def _get_object_and_image_points(self, nrows, ncols, image_set) -> tuple:
+        # termination criteria
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        # prepare object points, given chess board size
+        objp = np.zeros((nrows*ncols, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:ncols, 0:nrows].T.reshape(-1, 2)
+
+        # arrays to store object and image points
+        object_points, image_points = [], []
+
+        for fname in tqdm(image_set):
+            image = cv2.imread(fname)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            # find chess board corners
+            success, corners = cv2.findChessboardCorners(gray, (ncols, nrows), None)
+
+            if success:
+                object_points.append(objp)
+                # refine corners
+                corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+                image_points.append(corners2)
+        
+        return object_points, image_points
+        
+
+if __name__ == "__main__":
+    import glob
+    images_left = glob.glob("samples/calibration/*L*.jpg")
+    images_right = glob.glob("samples/calibration/*R*.jpg")
+    images_middle = glob.glob("samples/calibration/*M*.jpg")
+
+    calibrator = Calibrator(images_left, images_middle, images_right)
+    calibrator.calibrate()
+    print(calibrator.calibration_matrices)
