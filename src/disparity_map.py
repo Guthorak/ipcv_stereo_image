d0bc881abab67c3fef767395137c2e2972bd756b
Guthorak <Guthorakgaming@gmail.com>
2019-04-03 14:17:01 +0200

    Changed disparity_map class to disparity_map function

--- src/disparity_map.py
+++ src/disparity_map.py
@@ -4,8 +4,6 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-
-
 if __name__ == '__main__':
     
     image_left = cv2.imread('src/subject1_Left_1.jpg', cv2.IMREAD_GRAYSCALE)
@@ -20,31 +18,22 @@ if __name__ == '__main__':
     plt.imshow(disparity)
     plt.show()
     
+def DisparityMapper(image_left, image_right, num_desparities, box_size):
 
+    '''
+    Class for determining the disparity between two images
 
-class DisparityMapper:
-
-    def __init__(self, image_left, image_right, num_desparities, box_size):
-        '''
-        Class for determining the disparity between two images
-
-        Parameters:
-        -----------
-        image_left
-            image taken by left camera
-        image_right
-            image taken by right camera
-        num_disparity
-            disparity range. Class will find disparity for each pixel between 0 and num_disparity. Must be divisible by 16 (i think)
-        box_size
-            size of block compared by algorithm. Size should be odd as block is centered at current pixel
-        '''
-        self._images_left = image_left
-        self._images_right = image_right
-        self._num_desparities = num_desparities
-        self._box_size = box_size
-
-    def calculate_disparity(self):
-        stereo = cv2.StereoBM_create(self._num_desparities, self._box_size)
-        disparity_map = stereo.compute(self._images_left, self._images_right)
-        return disparity_map
+    Parameters:
+    -----------
+    image_left
+        image taken by left camera
+    image_right
+        image taken by right camera
+    num_disparity
+        disparity range. Class will find disparity for each pixel between 0 and num_disparity. Must be divisible by 16 (i think)
+    box_size
+        size of block compared by algorithm. Size should be odd as block is centered at current pixel
+    '''
+    stereo = cv2.StereoBM_create(num_desparities, box_size)
+    disparity_map = stereo.compute(image_left, image_right)
+    return disparity_map
