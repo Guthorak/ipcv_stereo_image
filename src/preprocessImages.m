92d64744cbf57f9fac0555e64adacf56b812e4dd
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-04-09 17:34:22 +0200

    Add mask rectification

--- src/preprocessImages.m
+++ src/preprocessImages.m
@@ -1,6 +1,8 @@
-function [left, right, mask] = preprocessImages(left_img, right_img, stereo_params)
-    [left, right] = rectifyStereoImages(rgb2gray(left_img), rgb2gray(right_img), stereo_params,...
-                                        'OutputView', 'valid');
-    [left, mask] = removeBackground(left);
-    [right, ~] = removeBackground(right);
+function [left, right, mask_l, mask_r] = preprocessImages(left_img, right_img, stereo_params)
+    [left, mask_l] = removeBackground(rgb2gray(left_img));
+    [right, mask_r] = removeBackground(rgb2gray(right_img));
+    [mask_l, mask_r] = rectifyStereoImages(mask_l, mask_r, stereo_params,...
+                                           'OutputView', 'full');
+    [left, right] = rectifyStereoImages(left, right, stereo_params,...
+                                        'OutputView', 'full');
 end
