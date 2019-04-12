4047fe8884136ba42cb72b9e4f21f30d3f3b8126
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-04-12 12:17:14 +0200

    Apply color to point clouds, use pcregistericp for point cloud alignment

--- src/mergePointClouds.m
+++ src/mergePointClouds.m
@@ -1,10 +1,8 @@
 function pc = mergePointClouds(pc1, pc2, grid_step, stereo_params1, stereo_params2)
     %[~, pc2_transformed] = pcregistericp(pc1, pc2);
-    tform1 = affine3d();
-    tform1.T(1:3, 1:3) = stereo_params1.RotationOfCamera2;
-    tform2 = affine3d();
-    tform2.T(1:3, 1:3) = inv(stereo_params2.RotationOfCamera2);
-    pc1_aligned = pctransform(pc1, tform1);
-    pc2_aligned = pctransform(pc2, tform2);
-    pc = pcmerge(pc1_aligned, pc2_aligned, grid_step);
+    tform = affine3d();
+    tform.T(1:3, 1:3) =  stereo_params2.RotationOfCamera2 * stereo_params1.RotationOfCamera2;
+    %pc1_aligned = pctransform(pc1, tform1); pc2_aligned = pctransform(pc2, tform2);
+    [~, pc1_aligned] = pcregistericp(pc1, pc2, 'MaxIterations', 10, 'InitialTransform', tform);
+    pc = pcmerge(pc1_aligned, pc2, grid_step);
 end
